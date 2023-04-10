b68aa80138b76d57af4366321bd76c7e11cfb578
austinhedrick548 <123927690+austinhedrick548@users.noreply.github.com>
2023-04-10 22:57:41 +0000

    Fixed unique key issue in console

--- frontend/src/components/TableFilters.tsx
+++ frontend/src/components/TableFilters.tsx
@@ -2,21 +2,22 @@ import * as React from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
-export default function ControlledCheckbox(restaurantName : string) {
-  const [checked, setChecked] = React.useState(true);
+export default function ControlledCheckbox(restaurantName: string) {
+    const [checked, setChecked] = React.useState(true);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-  };
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+    };
 
-  return (
-    <FormControlLabel
-      control={
-        <Checkbox checked={checked}
+    return (
+        <FormControlLabel
             key={restaurantName}
-            onChange={handleChange}
-            name={restaurantName} />}
-        label={restaurantName}
-    />
-  );
+            control={
+                <Checkbox checked={checked}
+
+                    onChange={handleChange}
+                    name={restaurantName} />}
+            label={restaurantName}
+        />
+    );
 }
