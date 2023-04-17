45016fca627e19186f6f51598eeb3f59d709a39c
austinhedrick548 <123927690+austinhedrick548@users.noreply.github.com>
2023-04-16 20:06:30 -0700

    Grid was defined but not used, so removed it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
