76d8d033d64e099135606534ebf8e967aa2345ef
kelseyp <kelsey.pritsker676@myci.csuci.edu>
2023-05-05 15:18:38 +0000

    Added data and formatting to dialog.

--- frontend/src/components/ItemDetailDialog.tsx
+++ frontend/src/components/ItemDetailDialog.tsx
@@ -1,17 +1,56 @@
-import { Dialog, DialogTitle } from "@mui/material";
+import { Card, CardContent, CardHeader, CardMedia, Dialog, Typography } from "@mui/material";
 
+import { useEffect, useState } from "react";
 import { MunchItem } from './MunchItem';
 
 export interface ItemDetailDialogProps {
-    open: boolean;
-    selectedItem: MunchItem;
-    handleDialogClose: () => void;
-  }
-  
-export function ItemDetailDialog(props: ItemDetailDialogProps) {  
-    return (
-      <Dialog  onClose={props.handleDialogClose} open={props.open}>
-        <DialogTitle>{props.selectedItem.item_name} Details</DialogTitle>
-      </Dialog>
-    );
-  }
+  open: boolean;
+  selectedItem: MunchItem;
+  handleDialogClose: () => void;
+}
+
+export function ItemDetailDialog(props: ItemDetailDialogProps) {
+  const loadingImage = require("../assets/images/spinning-loading.gif");
+  const [image, setImage] = useState(loadingImage);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      let response;
+      try {
+        response = await import(`../assets/images/${props.selectedItem.image}`);
+        setImage(response.default);
+      } catch (err) {
+        console.log(err);
+        setImage(require("../assets/images/404-error-page-not-found-with-two-men-plugging-in-cords.jpg"))
+      }
+    }
+    fetchImage()
+  }, [props.selectedItem.image])
+
+  return (
+    <Dialog onClose={props.handleDialogClose} open={props.open}>
+      <Card sx={{ width: 500 }}>
+        <CardHeader
+          title={props.selectedItem.name}
+          subheader={props.selectedItem.price}
+        />
+        <CardMedia
+          sx={{ height: 300 }}
+          image={image}
+          title={props.selectedItem.name}
+        />
+        <CardContent>
+          <Typography variant="subtitle1" color="text.secondary">
+            {props.selectedItem.description}
+          </Typography>
+          <Typography gutterBottom variant="body1" component="div">
+            {props.selectedItem.restaurant.name}
+          </Typography>
+          <Typography gutterBottom variant="body2" component="div">
+          {props.selectedItem.restaurant.address}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Dialog>
+  );
+}
