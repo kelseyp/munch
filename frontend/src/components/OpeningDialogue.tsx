c2859e941e2f4444cef794e36f248aa4ffcfa78d
NathanaelPaulus <123440471+NathanaelPaulus@users.noreply.github.com>
2023-05-09 02:41:23 +0000

    Added new dialogue, can't pass in searchbar

---
+++ frontend/src/components/OpeningDialogue.tsx
@@ -0,0 +1,47 @@
+import { SettingsInputComponent } from "@mui/icons-material";
+import { Card, CardContent, CardHeader, CardMedia, Dialog, Typography, DialogTitle, DialogContentText } from "@mui/material";
+import * as React from 'react';
+import { useEffect, useState } from "react";
+import { SearchBar } from './SearchBar';
+
+export interface OpeningDialog {
+    open : boolean;
+    search: SearchBar;
+    handleDialogClose: () => void;
+}
+
+export function OpeningDialog(props: OpeningDialog) {
+    const [open, setOpen] = React.useState(false);
+    
+    const handleClose = () => {
+            setOpen(true);
+        }
+
+    useEffect(() => {
+        
+    })
+
+    return (
+        <div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"What do you feel like eating?"}
+                </DialogTitle>
+                <DialogContentText>
+                        Type what you want to eat. It can be an item (hamburger, pizza, etc) 
+                        or a style of food (mexican, american, chinese, etc) or even a dietary restriction
+                        (Low card, vegan, etc) and Munch will tell you what to eat!
+                </DialogContentText>    
+                <div>
+                    {props.search}
+                </div>
+            </Dialog>
+        </div>
+    )
+
+}
